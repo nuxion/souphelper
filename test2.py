3d4d4c69e33191daef1c9b1e0fd76b4fbd7e7ffc
xavier petit <nuxion@gmail.com>
2017-02-24 09:43:52 -0300

    test2

---
+++ test2.py
@@ -0,0 +1,7 @@
+import souputils 
+
+
+uri = "https://www.pagina12.com.arhttps://www.pagina12.com.ar/22050-sturzenegger-admitio-que-los-tarifazos-ponen-presion-a-la-in"
+uri = "http://www.google.com"
+texto = souputils.getURL(uri)  
+print (texto)
